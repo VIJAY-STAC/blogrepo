41e88bca74f18e907cc27aef2dc48576a6f4499c
Vijay <vijay.thorat@biddano.com>
2023-10-15 03:09:40 +0530

    changed project path

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,10 +4,10 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/blogrepo/nginx/nginx.conf /etc/nginx/sites-available/blog
-sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
-# sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
-sudo nginx -t
-# sudo gpasswd -a www-data ubuntu
+sudo cp /home/ubuntu/blogrepo/nginx/nginx.conf /etc/nginx/sites-available/blog2
+sudo ln -s /etc/nginx/sites-available/blog2 /etc/nginx/sites-enabled/
+#sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
+#sudo nginx -t
+sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
 
