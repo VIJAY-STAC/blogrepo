2b9996ba894a12d2bcacf62e4b91ab6d02e69c0c
Vijay <vijay.thorat@biddano.com>
2023-10-15 01:29:55 +0530

    changed project path

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["65.0.129.136"]/' /home/ubuntu/blogprojectdrf/blog/settings.py
+sed -i 's/\[]/\["65.0.129.136"]/' /home/ubuntu/blogrepo/blog/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
