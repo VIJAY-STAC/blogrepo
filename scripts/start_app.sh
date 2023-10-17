9edfd1325e71c92394b6baa593fadb0a7e0c4444
Vijay <vijay.thorat@biddano.com>
2023-10-18 00:57:50 +0530

    updated cicd file

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["13.232.225.109"]/' /home/ubuntu/blogrepo/blog/settings.py
+pwd
+echo "hello1 "
+source /home/ubuntu/env/bin/activate
+echo "hello2 "
+pwd
+
+cd /home/ubuntu/blogrepo
+
+sed -i 's/\[]/\["13.232.225.109"]/'  /blog/settings.py
 
 python3 manage.py migrate 
 python3 manage.py makemigrations     
